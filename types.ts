ac29ff8c7e31ea980416224edf26e88974cda2a3
Arjun Bhusal <rhythemechristthelion@gmail.com>
2023-03-14 21:06:28 +0545

    learning basic typescript from thapatechnical

---
+++ types.ts
@@ -0,0 +1,85 @@
+// console.log("Hello World");
+// function subscribe(){
+//     for(var i=0;i<5;i++){
+//         console.log(i);
+//     }
+//         console.log("your block is"+i);
+//     }
+//     subscribe();
+// export{};
+    // let myNumber:number;
+    // myNumber=1;
+    // console.log(myNumber);
+// let anyType:any;
+// anyType="3";
+// console.log(anyType);
+
+// let arrString:string[];
+// arrString=["Arjun","Bhusal"];
+// console.log( arrString);
+
+// let anyType;
+// anyType=9;
+// document.write(anyType);
+
+//       Type Assertion
+// let personName;
+// personName="Arjun Bhusal";
+// (<string>personName).length;
+// (personName as string).length;
+
+//        Arrow Function
+// let personName=Bhusal()=>{
+//     console.log(Bhusal);
+// }
+
+//     Interface
+// let personName=(firstName:string,LastName)=>{
+//     console.log(firstName);
+// }
+// personName("Arjun","Bhusal");
+        //   &
+// let namets=(nameprof)=>{
+//     console.log(nameprof.firstName);
+// }
+// namets({
+//     firstName:"Arjun",
+//     lastName:"Bhusal"
+// })
+        //  &
+// let namets=(nameinfo:{firstName:number,lastName:string})=>{
+//     console.log(nameinfo.firstName);
+// }
+// namets({
+//     firstName:2,
+//     lastName:"Bhusal"
+// })
+        //  &
+// interface Nameinfo{
+//     firstName:string,
+//     lastName:string
+// }
+// let namets=(nameinfo:Nameinfo)=>{
+//     console.log(nameinfo.lastName);
+// }
+// namets({
+//     firstName:"Arjun",
+//     lastName:"Bhusal"
+// })
+
+//        Class and object
+// class person{
+//     firstName:String;
+//     lastName:String;
+//     age:Number;
+
+//     getfirstName(){
+//         console.log(this.firstName);
+//     }
+//     getlastName(){
+//         console.log(this.lastName);
+//     }
+// }
+// let personName=new person()
+// personName.firstName="Arjun";
+// personName.getfirstName();
